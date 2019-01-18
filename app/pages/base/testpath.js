1b62b1bd1ea2d2fc26007cedba79615c699f5450
1825929409 <13222192715@163.com>
2019-01-18 17:34:42 +0800

    1

--- app/pages/base/testpath.js
+++ app/pages/base/testpath.js
@@ -1,4 +1,4 @@
 const path1 = {"path1":"http://120.79.117.64:8383/weblink/",
-    "path2":"http://120.79.117.64/webLink/#/?targetId="
+    "path2":"http://120.79.117.64/webLink/transit.html?targetId="
 };
 export default path1;
